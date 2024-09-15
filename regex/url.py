7616cfa9267cc5038bb5fa982517beaab3ca1bf6
dahamkakooza <157519993+dahamkakooza@users.noreply.github.com>
2024-09-15 13:09:53 -0700

    Update url.py

--- regex/url.py
+++ regex/url.py
@@ -1,7 +1,15 @@
 import re
+
 def extract_data(text):
     data = {}
-# Regex pattern for matching URLs
-url_pattern = r'\b(?:https?://)?(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)?\b'
-# Find all matches in the text
-urls = re.findall(url_pattern, text)
+    
+    # Regex pattern for matching URLs
+    url_pattern = r'\b(?:https?://)?(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)?\b'
+    
+    # Find all matches in the text
+    urls = re.findall(url_pattern, text)
+    
+    # Extracted URLs in the dictionary is stored.
+    data['urls'] = urls
+    
+    return data
